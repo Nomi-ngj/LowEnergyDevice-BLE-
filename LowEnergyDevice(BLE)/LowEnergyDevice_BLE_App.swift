b1448c4e56754dde956f6280da7f3d0e0019c749
Nouman Gul <nouman@oasislivestock.com>
2025-03-07 12:54:29 +0500

    Initial Commit

---
+++ LowEnergyDevice(BLE)/LowEnergyDevice_BLE_App.swift
@@ -0,0 +1,17 @@
+//
+//  LowEnergyDevice_BLE_App.swift
+//  LowEnergyDevice(BLE)
+//
+//  Created by Rohit Kumar on 07/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct LowEnergyDevice_BLE_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
